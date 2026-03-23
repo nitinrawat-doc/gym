ba89fb79cc66d4d74bdef33373cd6d660dcf3984
nitinrawat-doc <nitinrawat2906@gmail.com>
2026-03-23 15:59:30 +0530

    fix API URL to render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api'
+  baseURL: 'https://true-fitness-api.onrender.com/api'
 });
 
-// Attach JWT token to every request
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('tf_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
-// Auto-logout on 401
 api.interceptors.response.use(
   res => res,
   err => {
